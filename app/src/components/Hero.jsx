79ccf76b6629c01a6f3b45b1bcfa40dbee2bf6d9
royalpinto007 <royalpinto007@gmail.com>
2023-11-08 15:30:41 +0530

    feat: hero section

---
+++ app/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+export default function Example() {
+  return (
+    <div className="bg-black px-10 pt-40 pr-40 pb-40">
+      <div className="text-right">
+        <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+          BharatGoDev
+        </h1>
+      </div>
+    </div>
+  );
+}
