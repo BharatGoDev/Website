9d56174f7bffa50d93a9ddd3b25051303cad6b82
royalpinto007 <royalpinto007@gmail.com>
2023-11-09 01:34:14 +0530

    feat: projects component

--- app/src/components/Hero.jsx
+++ app/src/components/Hero.jsx
@@ -1,8 +1,12 @@
 export default function Example() {
   return (
-    <div className="bg-black px-10 pt-20 pr-40 pb-40 pl-40">
+    <div className="bg-black px-10 pt-20 pr-40 pl-40">
       <div className="flex items-center">
-        <img src="/hero.svg" alt="hero" className="w-80 h-80 mt-0 hidden sm:block md:block" />
+        <img
+          src="/hero.svg"
+          alt="hero"
+          className="w-80 h-80 mt-0 hidden sm:block md:block"
+        />
         <div className="text-right pl-20 ml-20">
           <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">
             <span style={{ color: "#FF9933" }}>Bhar</span>
---
+++ app/src/components/Projects.jsx
@@ -0,0 +1,74 @@
+export default function Example() {
+  return (
+    <div className="bg-black">
+      <div className="mx-auto max-w-2xl pb-0 px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tight text-white">
+          {" "}
+          Projects
+        </h2>
+
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 lg:grid-cols-3 sm:grid-cols-2 xl:gap-x-8">
+          <div className="group relative h-full flex flex-col items-center">
+            {" "}
+            {/* Center text using flexbox */}
+            <div className="w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-50">
+              <img
+                src="./Stoccoin.jpeg"
+                alt="Stoccoin-Website"
+                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+              />
+            </div>
+            <div className="mt-4">
+              <h3 className="text-sm text-white">
+                <a href="https://github.com/Stoccoin-Official/Stoccoin-Website" target="blank">
+                  <span aria-hidden="true" className="absolute inset-0"></span>
+                  Stoccoin Website
+                </a>
+              </h3>
+            </div>
+          </div>
+
+          <div className="group relative h-full flex flex-col items-center">
+            {" "}
+            {/* Center text using flexbox */}
+            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-70">
+              <img
+                src="./Ember.jpeg"
+                alt="Ember"
+                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+              />
+            </div>
+            <div className="mt-4">
+              <h3 className="text-sm text-white">
+                <a href="https://github.com/EmberTechWorks/Ember" target="blank">
+                  <span aria-hidden="true" className="absolute inset-0"></span>
+                  Ember App
+                </a>
+              </h3>
+            </div>
+          </div>
+
+          <div className="group relative h-full flex flex-col items-center">
+            {" "}
+            {/* Center text using flexbox */}
+            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-70">
+              <img
+                src="Sevak.jpeg"
+                alt="Sevak"
+                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+              />
+            </div>
+            <div className="mt-4">
+              <h3 className="text-sm text-white">
+                <a href="https://github.com/Sevak-Official/Sevak" target="blank">
+                  <span aria-hidden="true" className="absolute inset-0"></span>
+                  Sevak Website
+                </a>
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
