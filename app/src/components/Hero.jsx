8588c86e37ac3d7a36cb5fa22ec0facef7481549
royalpinto007 <royalpinto007@gmail.com>
2023-11-08 19:26:40 +0530

    feat: customized hero

--- app/src/components/Hero.jsx
+++ app/src/components/Hero.jsx
@@ -1,10 +1,22 @@
 export default function Example() {
   return (
-    <div className="bg-black px-10 pt-40 pr-40 pb-40">
-      <div className="text-right">
-        <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-          BharatGoDev
-        </h1>
+    <div className="bg-black px-10 pt-20 pr-40 pb-40 pl-40">
+      <div className="flex items-center">
+        <img src="/hero.svg" alt="hero" className="w-80 h-80 mt-0" />
+        <div className="text-right pl-20 ml-20">
+          <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">
+            <span style={{ color: "#FF9933" }}>Bha</span>
+            <span style={{ color: "#FFFFFF" }}>ra</span>
+            <span style={{ color: "#000080" }}>t</span>
+            <span style={{ color: "#FFFFFF" }}>Go</span>
+            <span style={{ color: "#138808" }}>Dev</span>
+          </h1>
+          <p>
+            <div className="text-center pt-2">
+            <span style={{ color: "#FFFFFF"}}>Web & Graphic Design Specialists</span>
+            </div>
+          </p>
+        </div>
       </div>
     </div>
   );
