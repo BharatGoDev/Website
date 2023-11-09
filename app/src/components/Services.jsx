f4d22b5a25e8e41ecb1dd1f7408824750f72e210
targithu <githubtar@gmail.com>
2023-11-09 18:05:30 +0530

    fix : padding

--- app/src/components/Services.jsx
+++ app/src/components/Services.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline';
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 export default function Services() {
 
   const [imageIndex, setImageIndex] = useState(0);
@@ -24,26 +24,26 @@ export default function Services() {
         <div className="relative h-full w-full">
           <div className="absolute top-0 left-0 w-full h-full overflow-hidden">
             <div className="w-full h-full object-cover visible" style={{ transform: `translateX(-${imageIndex * 100}%)`, transition: 'transform 0.5s ease-in-out' }}>
-              <img src="../../public/Stoccoin.jpeg" alt="Stoccoin" />
+              <img src="/Stoccoin.jpeg" alt="Web Development" />
             </div>
             <div className="w-full h-full object-cover hidden" style={{ transform: `translateX(-${1 * 100}%)`, transition: 'transform 0.5s ease-in-out' }}>
-              <img src="../../public/Ember.jpeg" alt="Ember" />
+              <img src="/Ember.jpeg" alt="App Development" />
             </div>
             <div className="w-full h-full object-cover hidden" style={{ transform: `translateX(-${2 * 100}%)`, transition: 'transform 0.5s ease-in-out' }}>
-              <img src="../../public/Sevak.jpeg" alt="Sevak" />
+              <img src="/Sevak.jpeg" alt="Graphic Design" />
             </div>
           </div>
           <button
             className="absolute top-1/2 transform -translate-y-1/2 left-0 cursor-pointer bg-white hover:bg-gray-100 rounded-full p-2 md:hidden"
             onClick={handleBackClick}
           >
-            <ChevronLeftIcon className="h-6 w-6" />
+            <BsChevronCompactLeft className="h-6 w-6" />
           </button>
           <button
             className="absolute top-1/2 transform -translate-y-1/2 right-0 cursor-pointer bg-white hover:bg-gray-100 rounded-full p-2 md:hidden"
             onClick={handleNextClick}
           >
-            <ChevronRightIcon className="h-6 w-6" />
+            <BsChevronCompactRight className="h-6 w-6" />
           </button>
         </div>
       );
