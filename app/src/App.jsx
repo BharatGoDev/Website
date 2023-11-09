7e726df0f47c21596330aa115f9aeb6644db762f
targithu <githubtar@gmail.com>
2023-11-09 15:14:12 +0530

    feat:added form

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,6 +3,7 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+import ContactForm from "./components/Contact";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <Projects />
+      <ContactForm />
       <Footer />
     </div>
   );
--- app/src/components/Hero.jsx
+++ app/src/components/Hero.jsx
@@ -1,27 +1,26 @@
 export default function Example() {
   return (
-    <div className="bg-black px-10 pt-20 pr-40 pl-40">
-      <div className="flex items-center">
-        <img
-          src="/hero.svg"
-          alt="hero"
-          className="w-80 h-80 mt-0 hidden sm:block md:block"
-        />
-        <div className="text-right pl-20 ml-20">
-          <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">
-            <span style={{ color: "#FF9933" }}>Bhar</span>
-            <span style={{ color: "#FFFFFF" }}>atG</span>
-            <span style={{ color: "#138808" }}>oDev</span>
-          </h1>
-          <p>
-            <div className="text-center pt-2">
-              <span style={{ color: "#FFFFFF" }}>
-                Web & Graphic Design Specialists
-              </span>
-            </div>
-          </p>
-        </div>
+    <div className="bg-black lg:px-10 lg:pt-20 lg:pr-40 lg:pb-40 lg:pl-40">
+    <div className="flex items-center">
+      <img src="/hero.svg" alt="hero" className="lg:w-80 lg:h-80 lg:mt-0 hidden sm:hidden md:hidden lg:block" />
+      <div className="sm:text-center md:text-center text-right pl-20 ml-20 ">
+
+        <h1 className="text-4xl font-bold tracking-tight sm:text-6xl  ">
+          <span style={{ color: "#FF9933" }} >Bhar</span>
+          <span style={{ color: "#FFFFFF" }}>atG</span>
+          <span style={{ color: "#138808" }}>oDev</span>
+        </h1>
+
+        <p>
+          <div className="text-center pt-2">
+            <span style={{ color: "#FFFFFF" }}>
+              Web & Graphic Design Specialists
+            </span>
+          </div>
+        </p>
+
       </div>
     </div>
+  </div>
   );
 }
