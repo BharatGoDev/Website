d6985c5f1818fefad7f65d92f291a657bb3ec179
royalpinto007 <royalpinto007@gmail.com>
2023-11-10 10:44:13 +0530

    fix:  sign in

--- app/src/components/Auth.jsx
+++ app/src/components/Auth.jsx
@@ -26,35 +26,34 @@ export default function Auth() {
     setLoading(false);
   };
 
-  if (session) {
-    // User is signed in, hide the sign-in option
-    return <div>User is already signed in!</div>;
-  }
-
   return (
     <div className="row flex flex-center">
-      <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-        <h1 className="header pb-1 pt-1 text-sm text-gray-700">Sign in</h1>
-        <p className="description"></p>
-        <form className="form-widget" onSubmit={handleLogin}>
-          <div>
-            <input
-              className="inputField text-center pb-1 text-gray-700"
-              type="email"
-              placeholder="Enter your email"
-              value={email}
-              required={true}
-              onChange={(e) => setEmail(e.target.value)}
-              style={{ width: "150px"}}
-            />
-          </div>
-          <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-            <button className={'button block text-sm text-gray-700'} disabled={loading} style={{ textAlign: 'center', width: '100%', backgroundColor: '#000080', color: '#fff', borderRadius: '30%', padding: '10px' }}>
-              {loading ? <span>Loading</span> : <span>Send magic link</span>}
-            </button>
-          </div>
-        </form>
-      </div>
+      {session ? (
+        <div>User is already signed in!</div>
+      ) : (
+        <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <h1 className="header pb-1 pt-1 text-sm text-gray-700">Sign in</h1>
+          <p className="description"></p>
+          <form className="form-widget" onSubmit={handleLogin}>
+            <div>
+              <input
+                className="inputField text-center pb-1 text-gray-700"
+                type="email"
+                placeholder="Enter your email"
+                value={email}
+                required={true}
+                onChange={(e) => setEmail(e.target.value)}
+                style={{ width: "150px"}}
+              />
+            </div>
+            <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <button className={'button block text-sm text-gray-700'} disabled={loading} style={{ textAlign: 'center', width: '100%', backgroundColor: '#000080', color: '#fff', borderRadius: '30%', padding: '10px' }}>
+                {loading ? <span>Loading</span> : <span>Send magic link</span>}
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
