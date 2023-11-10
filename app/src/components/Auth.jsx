1e7f128856e095d749ff571c578953b900fa3243
royalpinto007 <royalpinto007@gmail.com>
2023-11-10 10:41:38 +0530

    fix: after sign in

--- app/src/components/Auth.jsx
+++ app/src/components/Auth.jsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { supabase } from "../supabaseClient";
 
 export default function Auth() {
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    // Check if there's an active session
+    const user = supabase.auth.user();
+    setSession(user);
+  }, []);
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -18,6 +25,11 @@ export default function Auth() {
     }
     setLoading(false);
   };
+
+  if (session) {
+    // User is signed in, hide the sign-in option
+    return <div>User is already signed in!</div>;
+  }
 
   return (
     <div className="row flex flex-center">
@@ -37,11 +49,10 @@ export default function Auth() {
             />
           </div>
           <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-  <button className={'button block text-sm text-gray-700'} disabled={loading} style={{ textAlign: 'center', width: '100%', backgroundColor: '#000080', color: '#fff', borderRadius: '30%', padding: '10px' }}>
-    {loading ? <span>Loading</span> : <span>Send magic link</span>}
-  </button>
-</div>
-
+            <button className={'button block text-sm text-gray-700'} disabled={loading} style={{ textAlign: 'center', width: '100%', backgroundColor: '#000080', color: '#fff', borderRadius: '30%', padding: '10px' }}>
+              {loading ? <span>Loading</span> : <span>Send magic link</span>}
+            </button>
+          </div>
         </form>
       </div>
     </div>
